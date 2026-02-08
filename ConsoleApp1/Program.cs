3925766d24b2184d622039a745314ba32ada24b0
Artem <artemmakarchuk1001@bk.ru>
2026-02-08 23:46:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a= ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}+{1}={1}+{0}", a, b);
+        }
+    }
+}
